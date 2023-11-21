097ec7781d6261a62a6f5c1ebaf5595e232585e9
ikhsanapriliano <ikhsanapriliano4@gmail.com>
2023-11-21 08:53:42 +0700

    3rd

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import cors from "cors";
 
 dotenv.config();
 const app = express();
-const port = 3000;
+const port = "https://bonfireserver.cyclic.app";
 const mongodbUri = process.env.MONGODB_URI;
 
 app.use(
